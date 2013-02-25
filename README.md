68835e3bca8d6a5dc8a1fad13f80ac4da38bf912
Kane Cohen <kanecohen@gmail.com>
2013-02-25 21:10:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# MultiSelectable
+
+## Simple jQuery plugin allowing selection/unselection of items
+
+-----
+
+## Usage
+
+````javascript
+$('ul').multiSelect();
+````
+
+-----
+
+## Options
+
+`multiple: true` (default) bool. Allow for selection of multiple elements in the list
+If set to false, you'll be able to select only one element.
+
+`selected: 'selected'` (default) string. Class that'll be assigned to selected item(s).
+
+`filter: '> *'` (default) string - selector. Selector that represents selectable items in the list.
+
+`unselectOn: false` (default) bool/selector/DOM element. Selector or DOM element which will trigger
+deselection of all selected items. 
+
+`keepSelection: true` (default) bool. That's a tricky option. Example: list with multiple items we selected holding `CTRL`.
+Now, if this option is set to false, then click on any of the elements without holding of CTRL will result in deselection of all other items.
+
+----
+
+## Callbacks
+
+Available callbacks:
+
+* `select`   - called before selection of the item. If returns `false`, then nothing will be selected
+* `unselect` - called when we click on the elements set in `unselectOn` option
+* `stop`     - called after selection and unselection of any item. First argument contains list of selected items
+Second argument is an item we selected right now
+
+How to use:
+
+````javascript
+$('ul').multiSelect().on('select', function(e, obj) {
+	// e - event
+	// obj - multiSelect object 
+}).on('stop', function(items, item) {
+	// callback fired after selection/unselection of the item	
+});
+````
---
+++ multiSelect-0.0.1.js
@@ -0,0 +1,130 @@
+(function($) {
+	$.fn.multiSelect = function(options) {
+		var o = $.extend(true, {}, options);
+		return this.each(function(k,v) {
+			o.element = $(this);
+			new MultiSelect(o);
+		});
+	};
+	function MultiSelect(o) {
+		this.init(o);
+	}
+	MultiSelect.prototype = {
+		d: {
+			multiple: true,
+			selected: 'selected',
+			filter:		' > *',
+			unselectOn:	   false,
+			keepSelection: true,
+			callbacks: {
+				select:   false, // args: event, this
+				unselect: false, // args: event, this
+				stop:     false //  args: selected items, current item
+			}
+		},
+		o: {},
+		init: function(options) {
+			$.extend(true, this.o, this.d, options);
+			this.initListeners();
+			this.initCallbacks();
+			this.o.element.data('multiSelect', this);
+		},
+		initCallbacks: function() {
+			var self = this;
+			$.each(this.o.callbacks, function(k, v) {
+				self.o.callbacks[k] = function() {
+					var args = Array.prototype.slice.call(arguments);
+					return self.o.element.triggerHandler(k, args);
+				};
+			});
+		},
+		initListeners: function() {
+			var self = this;
+			this.o.element.off('mousedown.multiSelect', this.o.filter);
+			this.o.element.on('mousedown.multiSelect', this.o.filter, function(e) {
+				if (e.which == 1) {
+					self.selectItems(e);
+				}
+				return true;
+			});
+			if (this.o.unselectOn) {
+				$(this.o.unselectOn).on('mousedown.multiSelect', function(e) {
+					if ( ! $(e.target).parents().is(self.o.element) && e.which != 3) {
+						if (self.o.trigger('unselect', e, self) === false) {
+							return false;
+						}
+						$(self.o.list+' .'+self.o.selected).removeClass(self.o.selected);
+						self.o.trigger('stop', o.element.find('.'+o.selected), o.element);
+					}
+				});
+			}
+		},
+		selectItems: function(e) {
+			var o = this.o,
+					target = e.target,
+					item   = $(e.currentTarget),
+					list   = this.o.element, first, last;
+
+			if (list.hasClass('ui-sortable-helper')) {
+				return false;
+			}
+			if (this.trigger('select', e, this) === false) {
+				return false;
+			}
+
+			if (e.shiftKey && o.multiple) {
+				item.addClass(o.selected);
+				first = list.find('.'+o.selected).first().index();
+				last = list.find('.'+o.selected).last().index();
+
+				// if we hold shift and try to select last element that is above current
+				if (first == -1 || last == -1) {
+					return false;
+				}
+				if (last < first) {
+					[first, last] = [last, first];
+				}
+				list.find('.'+o.selected).removeClass(o.selected);
+				var num = last - first;
+				for (var i=0;i<=num;i++) {
+					list.find(o.filter).eq(first+i).addClass(o.selected);
+				}
+				e.preventDefault();
+			} else if ((e.ctrlKey || e.metaKey) && o.multiple) {
+				// set/unset selection if we hold ctrl/meta key
+				item.toggleClass(o.selected);
+			} else {
+				// do not remove selection on second click on already selected item
+				if (o.keepSelection) {
+					if ( ! item.hasClass(o.selected) ) {
+						list.find('.'+o.selected).removeClass(o.selected);
+						item.addClass(o.selected);
+					}
+				} else {
+					list.find('.'+o.selected).removeClass(o.selected);
+					item.addClass(o.selected);
+				}
+			}
+			this.trigger('stop', o.element.find('.'+o.selected), o.element);
+		},
+		destroy: function() {
+			this.o.element.data('multiSelect', null);
+			this.o.element.off('.multiselect');
+			if (this.o.unselectOn) {
+				$(this.o.unselectOn).off('.multiselect');
+			}
+		},
+		trigger: function(name) {
+			var args = Array.prototype.slice.call(arguments, 1);
+			if (this.o.callbacks[name]) {
+				if (this.o.callbacks[name].apply(this, args) === false)
+					return false;
+			}
+			if (this[name]) {
+				if (this[name].apply(this, args) === false)
+					return false;
+			}
+			return true;
+		},
+	};
+})(jQuery);
